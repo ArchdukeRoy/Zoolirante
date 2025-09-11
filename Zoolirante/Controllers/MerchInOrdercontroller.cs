e3b1502729d385758a30a5eec2a63cba8318735c
coeay003 <aachelcoelho2004@gmail.com>
2025-09-11 19:37:11 +0930

    Add to cart

---
+++ Zoolirante/Controllers/MerchInOrdercontroller.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Zoolirante.Data;
+using Zoolirante.Models;
+
+namespace Zoolirante.Controllers
+{
+    public class MerchInOrdercontroller : Controller
+    {
+
+        private readonly ZooliranteContext _context;
+
+        public MerchInOrdercontroller(ZooliranteContext context)
+        {
+            _context = context;
+        }
+
+
+    }
+}
