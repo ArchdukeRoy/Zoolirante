b74c3341a088b18abbe7e0c7c9b67d10186bb00f
Roy Vallececera <royvallecera.01@gmail.com>
2025-09-16 01:22:26 +0930

    UI for Animal Details completed

--- Zoolirante/obj/Debug/net8.0/Zoolirante.AssemblyInfo.cs
+++ Zoolirante/obj/Debug/net8.0/Zoolirante.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Zoolirante")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1a984b995836a37ef73316c653455fbcd32e267")]
 [assembly: System.Reflection.AssemblyProductAttribute("Zoolirante")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Zoolirante")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
