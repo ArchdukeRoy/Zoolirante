b4b8fdb4c1f3d9044222234059e4db0773d5bcf3
bachie07 <giabach0704@gmail.com>
2025-09-19 13:23:30 +0930

    Ticket JS
    Add var for ticket page

---
+++ Zoolirante/wwwroot/js/ticket.js
@@ -0,0 +1,18 @@
+﻿// Simple variables
+var selectedDate = '';
+var selectedTime = '';
+var adults = 0;
+var children = 0;
+var concessions = 0;
+var cart = [];
+var currentMonth = 9; // October (0-based)
+var currentYear = 2024;
+
+// Prices
+var dayAdult = 39.99;
+var dayChild = 19.99;
+var dayConcession = 29.99;
+var unlimitedAdult = 59.98;
+var unlimitedChild = 28.98;
+var unlimitedConcession = 36.98;
+
