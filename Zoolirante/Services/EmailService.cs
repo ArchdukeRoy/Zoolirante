12fafc853200cccc23ca166de2c8c93ff89fa67a
bachie07 <giabach0704@gmail.com>
2025-10-09 23:38:42 +1030

    Create EmailService.cs

---
+++ Zoolirante/Services/EmailService.cs
@@ -0,0 +1,22 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+
+namespace Zoolirante.Services
+{
+    public interface IEmailService
+    {
+        Task SendTicketReceiptAsync(string toEmail, string customerName, List<TicketDetail> tickets, string sessionId);
+    }
+
+    public class EmailService : IEmailService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IQrCodeService _qrCodeService;
+
+        public EmailService(IConfiguration configuration, IQrCodeService qrCodeService)
+        {
+            _configuration = configuration;
+            _qrCodeService = qrCodeService;
+        }
+
+            bodyBuilder.HtmlBody = BuildTicketReceiptHtml(customerName, tickets, sessionId);
