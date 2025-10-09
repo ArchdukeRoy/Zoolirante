ec03a4eec9ac11a976b1db5599e06501dc256abd
bachie07 <giabach0704@gmail.com>
2025-10-09 23:38:03 +1030

    Add QR CodeService file

---
+++ Zoolirante/Services/QrCodeService.cs
@@ -0,0 +1,27 @@
+﻿using QRCoder;
+
+namespace Zoolirante.Services
+{
+    public interface IQrCodeService
+    {
+        string GenerateQrCodeBase64(string ticketId);
+    }
+
+    public class QrCodeService : IQrCodeService
+    {
+        public string GenerateQrCodeBase64(string ticketId)
+        {
+            string qrContent = $"https://zoolirante.com/verify/{ticketId}";
+
+            using (QRCodeGenerator qrGenerator = new QRCodeGenerator())
+            {
+                QRCodeData qrCodeData = qrGenerator.CreateQrCode(qrContent, QRCodeGenerator.ECCLevel.Q);
+                using (PngByteQRCode qrCode = new PngByteQRCode(qrCodeData))
+                {
+                    byte[] qrCodeBytes = qrCode.GetGraphic(20);
+                    return Convert.ToBase64String(qrCodeBytes);
+                }
+            }
+        }
+    }
+}
