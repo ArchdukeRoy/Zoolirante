0c38fa66af73b3daeda5becc90ef74edaf691257
bachie07 <giabach0704@gmail.com>
2025-10-09 23:39:25 +1030

    Implement SendTicketReceipt Async function

--- Zoolirante/Services/EmailService.cs
+++ Zoolirante/Services/EmailService.cs
@@ -19,4 +19,31 @@ namespace Zoolirante.Services
             _qrCodeService = qrCodeService;
         }
 
+        public async Task SendTicketReceiptAsync(string toEmail, string customerName, List<TicketDetail> tickets, string sessionId)
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Zoolirante Zoo", _configuration["Email:From"]));
+            message.To.Add(new MailboxAddress(customerName, toEmail));
+            message.Subject = "Your Zoolirante Ticket Receipt";
+
+            var bodyBuilder = new BodyBuilder();
             bodyBuilder.HtmlBody = BuildTicketReceiptHtml(customerName, tickets, sessionId);
+
+            message.Body = bodyBuilder.ToMessageBody();
+
+            using var client = new SmtpClient();
+            await client.ConnectAsync(
+                _configuration["Email:SmtpHost"],
+                int.Parse(_configuration["Email:SmtpPort"]),
+                MailKit.Security.SecureSocketOptions.StartTls
+            );
+
+            await client.AuthenticateAsync(
+                _configuration["Email:SmtpUser"],
+                _configuration["Email:SmtpPassword"]
+            );
+
+            await client.SendAsync(message);
+            await client.DisconnectAsync(true);
+        }
+
